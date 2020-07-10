3fbfbb776b8b2496bf8ff9b5ddd8d09a608e0614
EduardoCarruci <edugocarruci@gmail.com>
2020-07-10 18:50:12 -0400

    set

--- lib/src/widgets/login_form.dart
+++ lib/src/widgets/login_form.dart
@@ -92,6 +92,7 @@ class _LoginformState extends State<Loginform> {
                        ),
                        ), 
                      onPressed: widget.onGoToForgotPassword,
+                     //set
                      ),
                    ),
                      RounderdButton(
